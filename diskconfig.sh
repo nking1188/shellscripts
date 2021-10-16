538c708035a27e1b41ae2b9efbd1ad059de4e571
root user <root@domain.com>
2021-10-16 11:30:37 -0400

    First commit

---
+++ diskconfig.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+#This script creates a report of our disk configuration
+
+FILENAME= 'hostname'
+echo "Disk reprot saved to $FILENAME.report"
+
+echo -e "\n LVM Configuration: \n\n" >>$FILENAME.report
+lvscan >>$FILENAME.report
+
+echo -e "\n\n Partition Configuration:\n\n" >>$FILENAME.report
+fdisk -1 | head -17 >>$FILENAME.report 
+
+echo -e "\n\n Mounted Filesystems: \n\n" >>$FILENAME.report
+df -hT | grep -v tmp >>$FILENAME.report
+
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
