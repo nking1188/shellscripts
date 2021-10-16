62e334c7b657888078b69e208663c24edb273e28
root user <root@domain.com>
2021-10-16 11:45:09 -0400

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -15,3 +15,6 @@ df -hT | grep -v tmp >>$FILENAME.report
 
 echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
 mdadm --detail /dev/md0 >>$FILENAME.report
+
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
